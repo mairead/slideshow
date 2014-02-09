5683b16d611657cf0bdd5affc055cb0d50ee14c3
mairead <mdb.buchan@gmail.com>
2014-02-09 18:54:51 +0000

    Adding route to slides

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+app.get('/slides', function(req, res){
+  res.sendfile('./public/index.html');
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Emdeebeebee' });
 };
+
+exports.presentation = function(req, res){
+  res.sendfile('./public/index.html');
+};
